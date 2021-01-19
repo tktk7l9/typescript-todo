d14b574ab7880593de05a6a4fd231fa69fb7ee11
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-19 21:50:55 +0900

    ドラッグ & ドロップ機能の完成

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,18 @@ class ProjectState extends State<Project> {
             ProjectStatus.Active
         );
         this.projects.push(newProject);
+        this.updateListener();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListener();
+        }
+    }
+
+    private updateListener() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -237,8 +249,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
         }
      }
     
+    @autobind
     dropHandler(event: DragEvent) { 
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(
+            prjId,
+            this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished,
+        );
     }
     
     @autobind
