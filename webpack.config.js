1e926a286d8277b7d6557cd84b31a63ca0f8536c
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-20 23:37:57 +0900

    セットアップの完了 & webpack-dev-server の追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist',
+    },
+    devServer: {
+        publicPath: "dist",
     },
     devtool: 'inline-source-map',
     module: {
